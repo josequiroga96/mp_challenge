0fd05cb371ff506cfa1b07fc77d27358e58341ac
Jose Quiroga <jose.rojas@ing.austral.edu.ar>
2025-10-15 11:56:43 -0300

    Init project

---
+++ src/main/java/com/mp/challenge/ChallengeApplication.java
@@ -0,0 +1,24 @@
+package com.mp.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ChallengeApplication
+ * <p>
+ * ChallengeApplication class.
+ * <p>
+ * This application was built following my personal development standards.
+ * The code presented here is protected by intellectual property laws and copyrights.
+ *
+ * @author Jose Quiroga
+ * @since 14/10/2025
+ */
+@SpringBootApplication
+public class ChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeApplication.class, args);
+    }
+
+}
